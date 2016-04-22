8de4df49d8a51ee954105a8942312780dfff30f6
Kendall <kendallcar@csu.fullerton.edu>
2016-04-22 10:21:57 -0700

    updated

--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Zipcode(models.Model):
+class nflstats(models.Model):
     zip = models.PositiveIntegerField(primary_key=True)
     city =  models.CharField(max_length=27)
     state = models.CharField(max_length=2)
@@ -10,13 +10,14 @@ class Zipcode(models.Model):
     timezone = models.SmallIntegerField()
     daylight_savings_time = models.BooleanField(default=True)
 
-    def lat_lon_from_zip(self, zip):
-        geo_record = Zipcode.object.get(pk=zip)
-        if geo_record:
-            print("the type of the latitude is:")
-            print(type(geo_record.latitude))
-            return geo_record.latitude, geo_record.longitude
-        else:
-            print("zip not found: give CSUF zip")
-            return 33.640302, -117.769442
+    # def lat_lon_from_zip(self, zip):
+    #     geo_record = Zipcode.object.get(pk=zip)
+    #     if geo_record:
+    #         print("the type of the latitude is:")
+    #         print(type(geo_record.latitude))
+    #         return geo_record.latitude, geo_record.longitude
+    #     else:
+    #         print("zip not found: give CSUF zip")
+    #         return 33.640302, -117.769442
+
 
